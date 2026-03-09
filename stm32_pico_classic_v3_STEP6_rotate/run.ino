c28baced18187259709fbe602aad95eb06833970
aoki-rt <aoki@rt.net.jp>
2026-03-09 12:51:46 +0900

    add step6

---
+++ stm32_pico_classic_v3_STEP6_rotate/run.ino
@@ -0,0 +1,233 @@
+// Copyright 2025 RT Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+#include "run.h"
+
+RUN g_run;
+
+RUN::RUN() {
+  speed = 0.0;
+  accel = 0.0;
+  motor_move=false;
+}
+
+//割り込み
+void controlInterrupt(void) {
+  g_run.interrupt();
+}
+
+void stepRInterrupt(void){
+  g_run.interruptMotR();
+}
+
+void stepLInterrupt(void){
+  g_run.interruptMotL();
+}
+
+void RUN::interruptMotR(void){
+  if(g_run.motor_move){
+    Timer2->pause();
+    Timer2->setOverflow(g_run.step_hz_r, HERTZ_FORMAT);
+    Timer2->resume();     
+    
+    digitalWrite(PWM_R,HIGH);
+    for(int i=0;i<100;i++){
+      asm("nop \n");
+    }
+    digitalWrite(PWM_R,LOW);
+    g_run.step_r++;
+  }
+}
+
+void RUN::interruptMotL(void){
+  if(g_run.motor_move){
+    Timer3->pause();
+    Timer3->setOverflow(g_run.step_hz_l, HERTZ_FORMAT);
+    Timer3->resume();     
+    
+    digitalWrite(PWM_L,HIGH);
+    for(int i=0;i<100;i++){
+      asm("nop \n");
+    }
+    digitalWrite(PWM_L,LOW);
+    g_run.step_l++;
+  }
+}
+
+void RUN::interrupt(void) {  //割り込み内からコール
+
+  g_run.speed += g_run.accel;
+
+  if (g_run.speed > g_run.max_speed) {
+    g_run.speed = max_speed;
+  }
+  if (g_run.speed < g_run.min_speed) {
+    g_run.speed = g_run.min_speed;
+  }
+
+//   g_run.step_hz_l = g_run.step_hz_r = (unsigned short)(g_run.speed/PULSE);
+
+}
+
+void RUN::dirSet(t_CW_CCW dir_left, t_CW_CCW dir_right) {
+  if(dir_right==MOT_FORWARD){
+    digitalWrite(CW_R,LOW);
+  }else{
+    digitalWrite(CW_R,HIGH);
+  }
+  if(dir_right==MOT_FORWARD){
+    digitalWrite(CW_L,LOW);
+  }else{
+    digitalWrite(CW_L,HIGH);
+  }
+}
+
+void RUN::counterClear(void) {
+  g_run.step_r=g_run.step_l=0;
+}
+
+void RUN::speedSet(float l_speed, float r_speed) {
+  g_run.step_hz_r = r_speed/PULSE;
+  g_run.step_hz_l = l_speed/PULSE;
+}
+
+void RUN::stepGet(void) {
+  g_run.step_lr = g_run.step_r+g_run.step_l;
+  g_run.step_lr_len = (int)((float)g_run.step_lr / 2.0 * PULSE);
+}
+
+void RUN::stop(void) {
+  g_run.motor_move=0;
+}
+
+void RUN::accelerate(int len, int finish_speed) {
+  int obj_step;
+
+  g_run.accel = 1.5;
+  g_run.speed = g_run.min_speed = MIN_SPEED;
+  g_run.max_speed = finish_speed;
+  counterClear();
+  speedSet(MIN_SPEED,MIN_SPEED);
+  dirSet(MOT_FORWARD, MOT_FORWARD);
+  obj_step = (int)((float)len * 2.0 / PULSE);
+  g_run.motor_move=1;
+
+  while (1) {
+    stepGet();
+    speedSet(g_run.speed, g_run.speed);
+    if (g_run.step_lr > obj_step) {
+      break;
+    }
+  }
+}
+
+void RUN::oneStep(int len, int init_speed) {
+  int obj_step;
+
+  g_run.accel = 0.0;
+  g_run.max_speed = init_speed;
+  g_run.speed = g_run.min_speed = init_speed;
+  counterClear();
+  speedSet(init_speed,init_speed);   
+  dirSet(MOT_FORWARD, MOT_FORWARD);
+  obj_step = (int)((float)len * 2.0 / PULSE);
+
+  while (1) {
+    stepGet();
+    speedSet(g_run.speed, g_run.speed);
+    if (g_run.step_lr > obj_step) {
+      break;
+    }
+  }
+}
+
+void RUN::decelerate(int len, int init_speed) {
+  int obj_step;
+
+  g_run.accel = 1.5;
+  g_run.max_speed = init_speed;
+  g_run.speed = g_run.min_speed = init_speed;
+  counterClear();
+  speedSet(init_speed,init_speed);   
+  dirSet(MOT_FORWARD, MOT_FORWARD);
+  obj_step = (int)((float)len * 2.0 / PULSE);
+
+  while (1) {
+    stepGet();
+    speedSet(g_run.speed, g_run.speed);
+    if ((int)(len - g_run.step_lr_len) < (int)(((g_run.speed * g_run.speed) - (MIN_SPEED * MIN_SPEED)) / (2.0 * 1000.0 * g_run.accel))) {
+      break;
+    }
+  }
+
+  g_run.accel = -1.5;
+  g_run.min_speed = MIN_SPEED;
+
+  while (1) {
+    stepGet();
+    speedSet(g_run.speed, g_run.speed);
+    if (g_run.step_lr > obj_step) {
+      break;
+    }
+  }
+
+  stop();
+}
+
+
+void RUN::rotate(t_local_direction dir, int times)
+{
+  int obj_step;
+
+  g_run.accel = 1.5;
+  g_run.max_speed = 350.0;
+  g_run.speed = g_run.min_speed = MIN_SPEED;
+  counterClear();
+  speedSet(MIN_SPEED,MIN_SPEED);   
+  obj_step = (int)(TREAD_WIDTH * PI / 4.0 * (float)times * 2.0 / PULSE);
+
+  switch (dir) {
+    case right:
+      dirSet(MOT_FORWARD, MOT_BACK);    
+      break;
+    case left:
+      dirSet(MOT_BACK,MOT_FORWARD);
+      break;
+    default:
+      dirSet(MOT_FORWARD, MOT_FORWARD);
+      break;
+  }
+
+  while (1) {
+    stepGet();
+    speedSet(g_run.speed, g_run.speed);
+    if ((int)((obj_step/2.0*PULSE) - g_run.step_lr_len) < (int)(((g_run.speed * g_run.speed) - (MIN_SPEED * MIN_SPEED)) / (2.0 * 1000.0 * g_run.accel))) {
+      break;
+    }
+  }
+
+  accel = -1.5;
+  g_run.min_speed = MIN_SPEED;
+
+  while (1) {
+    stepGet();
+    speedSet(g_run.speed, g_run.speed);
+    if (g_run.step_lr > obj_step) {
+      break;
+    }
+  }
+
+  stop();
+
+}
+
---
+++ stm32_pico_classic_v3_STEP6_rotate/stm32_pico_classic_v3_STEP6_rotate.ino
@@ -0,0 +1,105 @@
+// Copyright 2025 RT Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+
+#include "run.h"
+
+HardwareTimer *Timer6 = new HardwareTimer(TIM6);
+HardwareTimer *Timer2 = new HardwareTimer(TIM2);//PWM_R PB10
+HardwareTimer *Timer3 = new HardwareTimer(TIM3);//PWM_L PA7
+
+#define LED0 PC11
+#define LED1 PC10
+#define LED2 PB15
+#define LED3 PB15
+
+#define SW_L PC15
+#define SW_C PF0
+#define SW_R PF1
+
+#define MOTOR_EN PC14
+#define CW_R PB1
+#define CW_L PC5
+#define PWM_R PB10
+#define PWM_L PA7
+
+#define MIN_HZ 80
+#define TIRE_DIAMETER (48.00)
+#define PULSE (TIRE_DIAMETER * PI / 400.0)
+#define TREAD_WIDTH (65.00)
+#define MIN_SPEED (MIN_HZ*PULSE)
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(LED0, OUTPUT);
+  pinMode(LED1, OUTPUT);
+  pinMode(LED2, OUTPUT);
+  pinMode(LED3, OUTPUT);
+
+  pinMode(SW_L, INPUT_PULLUP);
+  pinMode(SW_C, INPUT_PULLUP);
+  pinMode(SW_R, INPUT_PULLUP);
+
+  //motor disable
+  pinMode(MOTOR_EN, OUTPUT);
+  pinMode(CW_R, OUTPUT);
+  pinMode(CW_L, OUTPUT);
+  pinMode(PWM_R, OUTPUT);
+  pinMode(PWM_L, OUTPUT);
+
+  digitalWrite(MOTOR_EN, LOW);
+  digitalWrite(CW_R, LOW);
+  digitalWrite(CW_L, LOW);
+  digitalWrite(PWM_R, LOW);
+  digitalWrite(PWM_L, LOW);
+
+  Timer6->pause();
+  Timer6->setOverflow(1000, MICROSEC_FORMAT);
+  Timer6->attachInterrupt(controlInterrupt);
+  Timer6->refresh();
+  Timer6->resume();  //Timter Start
+
+  Timer2->pause();
+  Timer2->setOverflow(MIN_HZ, HERTZ_FORMAT);
+  Timer2->attachInterrupt(stepRInterrupt);
+  Timer2->refresh();
+  Timer2->resume();  //Timter Start
+
+  Timer3->pause();
+  Timer3->setOverflow(MIN_HZ, HERTZ_FORMAT);
+  Timer3->attachInterrupt(stepLInterrupt);
+  Timer3->refresh();
+  Timer3->resume();  //Timter Start
+
+
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  while (digitalRead(SW_L) & digitalRead(SW_C) & digitalRead(SW_R)) {
+    continue;
+  }
+  digitalWrite(MOTOR_EN, HIGH);
+  delay(1000);
+  g_run.rotate(right, 1);
+  delay(1000);
+  g_run.rotate(left, 1);
+  delay(1000);
+  g_run.rotate(right, 2);
+  delay(1000);
+  g_run.rotate(left, 2);
+  delay(1000);
+  digitalWrite(MOTOR_EN, LOW);
+}
